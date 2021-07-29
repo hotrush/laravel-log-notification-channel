649d2c5c8adcfbf6a12d5913e76c7931a2921bfc
Enaah <mathis.bin@icloud.com>
2021-07-29 12:18:16 +0200

    fix/"Argument #1 must be type of object"
    - fixes the Error that can occur " get_class(): Argument #1 ($object) must be of type object, null given"
    - Error happens (from) on PHP8 (7.4 was working fine)

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -13,10 +13,11 @@ class CouldNotSendNotification extends \Exception
 
     public static function invalidMessageObject($message)
     {
-        $className = get_class($message) ?: 'Unknown';
+        $className = is_object($message) ? get_class($message) : 'Unknown';
 
         return new static(
-            "Notification was not sent. Message object class `{$className}` is invalid. 
-            It should be `".LogMessage::class.'`');
+            "Notification was not sent. Message object class `{$className}` is invalid.
+            It should be `" . LogMessage::class . '`'
+        );
     }
 }
